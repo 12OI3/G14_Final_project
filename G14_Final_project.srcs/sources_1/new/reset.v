7de6aad31ddc6990a9f2c0a0873e2c768ba25eff
ROB <benbook90@gmail.com>
2022-01-14 16:59:00 +0800

    reset.v improve

--- G14_Final_project.srcs/sources_1/new/reset.v
+++ G14_Final_project.srcs/sources_1/new/reset.v
@@ -4,14 +4,15 @@ module reset (
     output reg rst_p1_position_x,
     output reg rst_p1_position_y,
     output reg [97:0] rst_board,
+    output reg [2:0] s0_star_x,
+    output reg [2:0] s0_star_y,
     output reg [2:0] s1_star_x,
     output reg [2:0] s1_star_y,
     output reg [2:0] s2_star_x,
     output reg [2:0] s2_star_y,
     output reg [2:0] s3_star_x,
-    output reg [2:0] s3_star_y,
-    output reg [2:0] s4_star_x,
-    output reg [2:0] s4_star_y
+    output reg [2:0] s3_star_y
+    
     );
 
     always @(*) begin
@@ -24,16 +25,17 @@ module reset (
                     2'b00, 2'b00, 2'b00, 2'b00, 2'b00, 2'b00, 2'b00,
                     2'b00, 2'b01, 2'b00, 2'b00, 2'b00, 2'b01, 2'b00,
                     2'b00, 2'b00, 2'b00, 2'b00, 2'b00, 2'b00, 2'b00};
-        s1_star_x = 0;
-        s1_star_y = 3;
+                    
+        s0_star_x = 0;
+        s0_star_y = 3;
 
-        s2_star_x = 2;
-        s2_star_y = 0;
+        s1_star_x = 2;
+        s1_star_y = 0;
+
+        s2_star_x = 5;
+        s2_star_y = 1;
 
         s3_star_x = 5;
-        s3_star_y = 1;
-
-        s4_star_x = 5;
-        s4_star_y = 5;
+        s3_star_y = 5;
     end
 endmodule
